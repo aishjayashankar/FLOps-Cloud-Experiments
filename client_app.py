98f9e3ecda4e4a6aa685ca5249915583618d8d8e
Aishwarya Jayashankar <aishwaryajayashankarvit@gmail.com>
2025-05-12 18:00:05 +0000

    Niid drop only client 4

--- client_app.py
+++ client_app.py
@@ -10,11 +10,11 @@ from flops_infra_drift.task import Net, get_weights, load_data, set_weights, tes
 from collections import OrderedDict
 
 def ShouldNodeDisconnect(partition_id, current_round):
-        if (partition_id < 2):
+        if (partition_id != 3):
             return False
         # For node n, partition_id is n-1
         # start_disconnect = 5, 6, 7 for partition_ids 2, 3, 4
-        start_disconnect = (partition_id + 3)
+        start_disconnect = 7 #(partition_id + 3)
         end_disconnect = 31
 
         return start_disconnect <= current_round < end_disconnect
