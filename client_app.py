07f76021472fedab951810be38693c5a44989aaa
Ketan Atri <atriketan1@gmail.com>
2025-11-30 18:35:07 +0530

    Add logging mechanism

--- client_app.py
+++ client_app.py
@@ -11,6 +11,10 @@ from flops_infra_drift.subset_client_trainer import get_subset_client_trainer
 from flops_infra_drift.task import load_data, test, train, Net
 from flwr.client import ClientApp, NumPyClient
 from flwr.common import Context
+from flops_infra_drift.task import Net, get_weights, load_data, set_weights, test, train
+from flops_infra_drift.logger_config import configure_logging
+from collections import OrderedDict
+
 
 def ShouldNodeDisconnect(partition_id, current_round):
     if partition_id not in consts.DROPPED_CLIENT_PARITIONS_IDS:
@@ -103,7 +107,7 @@ class FlowerClient(NumPyClient):
         loss, accuracy = test(self.model, self.valloader, self.device)
         end_time = time.time()
         runtime = end_time - start_time
-        print(f"Client: {self.partition_id} took {runtime:.4f} seconds to evaluate.")
+        logging.info(f"Client: {self.partition_id} took {runtime:.4f} seconds to evaluate.")
         return loss, len(self.valloader.dataset), {"accuracy": accuracy}
 
     def get_label_distribution(self) -> dict:
